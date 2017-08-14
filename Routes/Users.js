5e98a5252acd5ab0b27e1bc5037cfe870455d08b
Devesh vyas <dev2605vyas@gmail.com>
2017-08-14 14:46:28 +0530

    added jwt token

--- Routes/Users.js
+++ Routes/Users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var users = express.Router();
 var database = require('../Database/database');
 var cors = require('cors')
+var jwt = require('jsonwebtoken');
+var token;
 
 users.use(cors());
+
+process.env.SECRET_KEY = "devesh";
 
 users.post('/register', function(req, res) {
 
@@ -43,10 +47,7 @@ users.post('/register', function(req, res) {
 
 users.post('/login', function(req, res) {
 
-    var appData = {
-        "error": 1,
-        "data": ""
-    };
+    var appData = {};
     var email = req.body.email;
     var password = req.body.password;
 
@@ -64,8 +65,11 @@ users.post('/login', function(req, res) {
                 } else {
                     if (rows.length > 0) {
                         if (rows[0].password == password) {
+                            let token = jwt.sign(rows[0], process.env.SECRET_KEY, {
+                                expiresIn: 3600000
+                            });
                             appData.error = 0;
-                            appData["data"] = "Login Successfull!";
+                            appData["token"] = token;
                             res.status(200).json(appData);
                         } else {
                             appData.error = 1;
@@ -82,6 +86,88 @@ users.post('/login', function(req, res) {
             connection.release();
         }
     });
+});
+
+users.post('/login', function(req, res) {
+
+    var appData = {};
+    var email = req.body.email;
+    var password = req.body.password;
+
+    database.connection.getConnection(function(err, connection) {
+        if (err) {
+            appData["error"] = 1;
+            appData["data"] = "Internal Server Error";
+            res.status(500).json(appData);
+        } else {
+            connection.query('SELECT * FROM users WHERE email = ?', [email], function(err, rows, fields) {
+                if (err) {
+                    appData.error = 1;
+                    appData["data"] = "Error Occured!";
+                    res.status(400).json(appData);
+                } else {
+                    if (rows.length > 0) {
+                        if (rows[0].password == password) {
+                            token = jwt.sign(rows[0], process.env.SECRET_KEY, {
+                                expiresIn: 5000
+                            });
+                            appData.error = 0;
+                            appData["token"] = token;
+                            res.status(200).json(appData);
+                        } else {
+                            appData.error = 1;
+                            appData["data"] = "Email and Password does not match";
+                            res.status(204).json(appData);
+                        }
+                    } else {
+                        appData.error = 1;
+                        appData["data"] = "Email does not exists!";
+                        res.status(204).json(appData);
+                    }
+                }
+            });
+            connection.release();
+        }
+    });
+});
+
+users.get('/getUsers', function(req, res) {
+
+    var appData = {};
+    var token = req.body.token || req.headers['token'];
+    if (token) {
+        jwt.verify(token, process.env.SECRET_KEY, function(err) {
+            if (err) {
+                appData["error"] = 1;
+                appData["data"] = "Token is invalid";
+                res.status(500).json(appData);
+            } else {
+                database.connection.getConnection(function(err, connection) {
+                    if (err) {
+                        appData["error"] = 1;
+                        appData["data"] = "Internal Server Error";
+                        res.status(500).json(appData);
+                    } else {
+                        connection.query('SELECT *FROM users', function(err, rows, fields) {
+                            if (!err) {
+                                appData["error"] = 0;
+                                appData["data"] = rows;
+                                res.status(200).json(appData);
+                            } else {
+                                appData["data"] = "No data found";
+                                res.status(204).json(appData);
+                            }
+                        });
+                        connection.release();
+                    }
+                });
+            }
+        });
+    } else {
+        appData["error"] = 1;
+        appData["data"] = "Please send a token";
+        res.status(200).json(appData);
+    }
 });
 
 module.exports = users;
